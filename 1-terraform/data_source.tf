7633960f8ea33109c81ccccff6ae8fcac0124e83
amitvgi12 <52849460+amitvgi12@users.noreply.github.com>
2020-04-05 11:37:12 +0530

    Update data_source.tf

--- 1-terraform/data_source.tf
+++ 1-terraform/data_source.tf
@@ -9,10 +9,10 @@ data "azurerm_key_vault_secret" "ssh_public_key" {
 }
 
 data "azurerm_key_vault_secret" "spn_id1" {
-  name         = "spn-id"
+  name         = "spn-id1"
   key_vault_id = data.azurerm_key_vault.terraform_vault.id
 }
 data "azurerm_key_vault_secret" "spn_secret1" {
-  name         = "spn-secret"
+  name         = "spn-secret1"
   key_vault_id = data.azurerm_key_vault.terraform_vault.id
 }
